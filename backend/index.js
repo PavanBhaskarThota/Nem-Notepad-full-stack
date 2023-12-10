31d123e51e4cc173887215d0da923f9a55eb0864
PavanBhaskarThota <pavanbhaskarthota05@gmail.com>
2023-12-11 01:25:32 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,19 @@ const express = require("express");
 const { connection } = require("./DataBase/db");
 const { userRouter } = require("./Routes/user.router");
 const { noteRouter } = require("./Routes/note.router");
+const cors = require("cors")
 
 const app = express();
 
 app.use(express.json());
+app.use(cors)
 app.use("/user", userRouter);
 app.use("/note", noteRouter);
 
 
-app.listen(4500, async () => {
+app.listen(5500, async () => {
   try {
     await connection;
-    console.log("Sever at 4500 and DataBase is connected");
+    console.log("Sever at 5500 and DataBase is connected");
   } catch (error) {}
 });
